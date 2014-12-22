d36463e216799c86302022dbb4a296965d66c7d9
rob <bookbob83@gmail.com>
2014-12-22 00:45:51 -0800

    adding new files

---
+++ app/src/main/java/app/subversive/groceryratings/UI/IconDrawable.java
@@ -0,0 +1,12 @@
+package app.subversive.groceryratings.UI;
+
+import android.graphics.drawable.BitmapDrawable;
+
+/**
+ * Created by rob on 11/22/14.
+ */
+public class IconDrawable extends BitmapDrawable {
+    public IconDrawable() {
+        super();
+    }
+}
